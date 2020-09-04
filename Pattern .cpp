0bc60a12cd0458041bce1aa9c0a0473643eb4b10
Pawanvyas <67539195+Pawanvyas@users.noreply.github.com>
2020-09-04 20:36:38 +0530

    Add files via upload

---
+++ Pattern .cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+ 
+   int n,i;
+   printf("enter any number");
+   scanf("%d",&n);
+   for(i=1;i<=n;i++) 
+     {
+           printf("*\n");
+           
+     }
+ 
+
+}
