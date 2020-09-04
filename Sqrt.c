eb505391a95167ed03ec9cba6d6b19a3af9dba45
Pawanvyas <67539195+Pawanvyas@users.noreply.github.com>
2020-09-04 20:36:58 +0530

    Add files via upload

---
+++ Sqrt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+void main()
+{
+ int i=0;
+ float a,b;
+ printf("number   sqrt   square\n");
+ while(i<=100)
+ {
+  a=sqrt(i);
+  b=i*i;
+  printf("%d   %f   %f\n",i,a,b);
+  i=i+1;
+  }
+}
+  
+
+ 
+ 
+   
