6365d3d549dc1023048cd667fbd9605593e79c5a
Pawanvyas <67539195+Pawanvyas@users.noreply.github.com>
2020-09-04 20:32:22 +0530

    Add files via upload

---
+++ Sum.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+ 
+  int a,b,c; 
+  printf("enter the value a");
+  scanf("%d",&a);
+  printf("enter the value b");
+  scanf("%d",&b);
+
+  c= a+b;
+  printf("%d",c);                                                                                    
+}
