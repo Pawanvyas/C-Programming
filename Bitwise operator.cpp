cf9dd429d7e52797eea1802c9c121bbc4b70e3b0
Pawanvyas <67539195+Pawanvyas@users.noreply.github.com>
2020-09-04 20:33:03 +0530

    Add files via upload

---
+++ Bitwise operator.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int a=6,b=2; 
+
+  printf("\n%dAND %d = %d",a,b,a&b);
+  printf("\n%dOR  %d = %d",a,b,a|b);
+  printf("\n%dXOR %d = %d",a,b,a^b);
+
+  printf("\n 5<<2 = %d",5<<2);
+  printf("\n 5>>2 =%d",5>>2);
+  getch;
+
+}
