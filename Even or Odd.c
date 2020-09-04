f70c2c197229e9d4f35a13eb97f5ab79911229fb
Pawanvyas <67539195+Pawanvyas@users.noreply.github.com>
2020-09-04 20:34:58 +0530

    Add files via upload

---
+++ Even or Odd.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+/*****************************************************
+ Author kunal thanvi
+ Purpose To find number from user and check even or odd  
+******************************************************/
+void main()
+{
+   //Variable Declration
+   int input; 
+  
+   // Read value from user  
+   printf("Enter any number:");  
+   scanf("%d",&input);
+
+   //Logic
+    if(inp
+                              
+%2==0)
+       printf("%d is an even number",input);
+    else
+       printf("%d ian odd number",input);
+    getch();   
+}     
