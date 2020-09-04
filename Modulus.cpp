9b47814485681583946fba5546332b20bc3025c9
Pawanvyas <67539195+Pawanvyas@users.noreply.github.com>
2020-09-04 20:35:49 +0530

    Add files via upload

---
+++ Modulus.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int a,b,c;
+  a=10;
+  b=12;
+  c=a%b;
+  printf("enter the numbers");
+  scanf("%d %d",&a,&b);
+  
+  c=a%b;
+  printf("%d",c);
+}
